6a0a430ab2d3294bfca0de71430bca2fb0df2796
Egoscio <theiqboy@gmail.com>
2016-02-24 11:33:10 -0800

    Removed everything
    This repository now only hosts code rendered by pages-code

--- script.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// script.js
-
-function loadPage(page, success, error) {
-  success = success || function () {};
-  error = error || function () {};
-  $.get(page + '.html', success).fail(error);
-}
--- src/babel/script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-
-function loadPage(page, success, error) {
-  success = success || function() {}
-  error = error || function() {}
-  $.get(page + '.html', success).fail(error)
-}
