5c6775652cc9f1738a55ee04a58263dcf50723f4
Egoscio <theiqboy@gmail.com>
2016-05-16 12:02:14 -0700

    Added changes

--- webapp/script.js
+++ webapp/script.js
@@ -7,16 +7,37 @@ $(document).ready(function () {
     red: 0,
     blue: 0
   }
+  var won = false
+  var $taps = $('.tap')
 
-  $('.tap').on('touchstart mousedown', function (e) {
-    // The element that was clicked:
-    var $this = $(this)
-    // Grabs the second item of the class attribute, which is red or blue respectively.
-    var type = $this.attr('class').split(' ')[1]
-    // Increments the score
-    scores[type] += 1
-    // Change the text.
-    $this.text('Your score is ' + scores[type])
+  // When someone taps an element with the class "tap", run the function.
+  $taps.on('touchstart mousedown', function (e) {
+    if (!won) {
+      // The element that was clicked:
+      var $this = $(this)
+      // Grabs the second item of the class attribute, which is red or blue respectively.
+      var type = $this.attr('class').split(' ')[1]
+      // Increments the score
+      scores[type] += 1
+      // Change the text.
+      $this.text('Your score is ' + scores[type])
+      // Add condition for finishing the game.
+      if (scores[type] >= 100) {
+        won = true
+        // Reward the winner.
+        $taps.text(type + ' won the game!')
+        setTimeout(function () {
+          // Reset scores
+          scores = {
+            red: 0,
+            blue: 0
+          }
+          // Add it back.
+          $taps.text('Tap to play')
+          won = false
+        }, 3000)
+      }
+    }
     // Stop it from doing generic actions like selecting text.
     return false
   })
